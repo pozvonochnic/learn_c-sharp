5e26146f2c5fe146312440df9841a692f91e16fc
pozvonochnic <pozvonochnic@gmail.com>
2022-12-26 17:52:59 +0300

    code edited

--- Test/Task/Program.cs
+++ Test/Task/Program.cs
@@ -1,31 +1,9 @@
 ﻿using System;
 using static System.Console;
 
-//Clear();
-// string [] stringArray = new string[6];
-// stringArray[0] = "asd;fkjh";
-// stringArray[1] = String.Empty;
-// stringArray[2] = "hdj";
-// stringArray[3] = String.Empty;
-// stringArray[4] = "Ээээ";
-// stringArray[5] = "hdfjk";
-// WriteLine(String.Join(" | ", stringArray));
 
-// int count = 0;
-// foreach (string item in stringArray)
-// {
-//     if (item != String.Empty)
-//         count++;
-// }
-// WriteLine(count);
-
-// int num1 = stringArray[0].Length;
-// int num2 = stringArray[1].Length;
-// WriteLine(num1 + " " + num2);
-
-// int [] nullArray = new int [0];
-
-// WriteLine("["+String.Join(",", nullArray)+"]");
+Clear();
+            //VARIANT 1
 
 
 int CountElements (string [] array)
@@ -50,26 +28,31 @@ string [] FillStringArray(string[]array1, string[]array2)
 }
 
 WriteLine("Введите строковые элементы массива через запятую:");
-//string elements = ReadLine();
-string [] stringArray = ReadLine().Split(',');
-
-// string newString = String.Empty;
-// newString = $"{newString}{stringArray[0]}";
-// WriteLine(newString);
-// newString = $"{newString}{stringArray[1]}";
-// WriteLine(newString);
+string [] stringArray = ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
 
 int newSize = CountElements(stringArray);
 string [] newStringArray = new string [newSize];
 newStringArray = FillStringArray(stringArray, newStringArray);
 
-// Оформить в метод
-
 WriteLine("["+String.Join(", ", newStringArray)+"]");
 
-//Write(newSize);
 
-// 0. Задать массив строкой через разделитель.
-// 1. Метод. Сплитонуть. Посчитать количество нужных элементов. Это будет размер нового массива - возвратить.
-// 2. Метод (аргументы: исходный массив, размер нового массива). Заполнить новый массив через foreach (возможно) и if.
-// 3. Вывести новый массив.
+
+            //VARIANT 2
+
+/*
+string [] ChoseElements (string [] array)
+{
+    string newString = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+            newString = $"{newString},{array[i]}";
+    string [] newStringArray = newString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+    return newStringArray;
+}
+
+WriteLine("Введите строковые элементы массива через запятую:");
+string [] stringArrayVar2 = ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+string[] newArray = ChoseElements(stringArrayVar2);
+WriteLine("["+String.Join(", ", newArray)+"]");
+*/
